340e5383c56c09a3ab1556ea93966c15636cf1e5
Olarewaju Opeyemi <olarewajuakeemopeyemi@gmail.com>
2017-01-12 15:26:50 +0100

    added js

---
+++ js/userAuthentication.js
@@ -0,0 +1,13 @@
+function UserAuthentication(username, password) {  //to be edited
+    var theUsername = document.userform.username;
+    var thePassword = document.userform.password;
+
+    var userDetails = ['Olaitan Bakare', 'thisPassword'];
+    if(theUsername === userDetails[1] && thePassword === userDetails[2]){
+         return salaryCalculator.html; //to be edited
+    }else{
+    	return 'wrong login details'
+    }
+  }
+
+  function 
